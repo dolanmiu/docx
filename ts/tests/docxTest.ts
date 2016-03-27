5c419cd3c4e6e42941265dc87577f883eeccaadc
Dolan Miu <dolan_miu@hotmail.com>
2016-03-27 03:09:50 +0100

    added tests

---
+++ ts/tests/docxTest.ts
@@ -0,0 +1,21 @@
+/// <reference path="../docx/docx.ts" />
+/// <reference path="../typings/mocha/mocha.d.ts" />
+describe('Calculator', () => {
+    var document : docx.Docx;
+
+    /*beforeEach(function () {
+        subject = new Calculator();
+    });
+
+    describe('#add', () => {
+        it('should add two numbers together', () => {
+            var result : number = subject.add(2, 3);
+            if (result !== 5) {
+                throw new Error('Expected 2 + 3 = 5 but was ' + result);
+            }
+        });
+    });*/
+    describe('#test', () => {
+        console.log(new docx.Docx);
+    });
+});
