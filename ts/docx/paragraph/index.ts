9e8fd9739de1ef4338b0ee9164cfbc09b2018c7a
Dolan Miu <dolan_miu@hotmail.com>
2016-03-30 04:31:53 +0100

    made return values for methods

--- ts/docx/paragraph/index.ts
+++ ts/docx/paragraph/index.ts
@@ -30,77 +30,77 @@ export class Paragraph {
         this.p.push(new TextRun(text));
     }
 
-    addText(run: TextRun) {
+    addText(run: TextRun): Paragraph {
         this.p.push(run);
         return this;
     }
 
-    heading1() {
+    heading1(): Paragraph {
         this.properties.push(new Style("Heading1"));
         return this;
     }
 
-    heading2() {
+    heading2(): Paragraph {
         this.properties.push(new Style("Heading2"));
         return this;
     }
 
-    heading3() {
+    heading3(): Paragraph {
         this.properties.push(new Style("Heading3"));
         return this;
     }
 
-    heading4() {
+    heading4(): Paragraph {
         this.properties.push(new Style("Heading4"));
         return this;
     }
 
-    heading5() {
+    heading5(): Paragraph {
         this.properties.push(new Style("Heading5"));
         return this;
     }
 
-    title() {
+    title(): Paragraph {
         this.properties.push(new Style("Title"));
         return this;
     }
 
-    center() {
+    center(): Paragraph {
         this.properties.push(new Alignment("center"));
         return this;
     }
 
-    left() {
+    left(): Paragraph {
         this.properties.push(new Alignment("left"));
         return this;
     }
 
-    right() {
+    right(): Paragraph {
         this.properties.push(new Alignment("right"));
         return this;
     }
 
-    justified() {
+    justified(): Paragraph {
         this.properties.push(new Alignment("both"));
         return this;
     }
 
-    thematicBreak() {
+    thematicBreak(): Paragraph {
         this.properties.push(new ThematicBreak());
         return this;
     }
 
-    pageBreak() {
+    pageBreak(): Paragraph {
         this.properties.push(new PageBreak());
         return this;
     }
 
-    addTabStop(tabStop: TabStop) {
+    addTabStop(tabStop: TabStop): Paragraph {
         this.properties.push(tabStop);
         return this;
     }
 
-    bullet() {
+    bullet(): Paragraph {
         this.properties.push(new Style("ListParagraph"));
         this.properties.push(new NumberProperties());
         return this;
