9b6f6082f5284c5f09d487b51499ed1117481c50
Dolan Miu <dolan_miu@hotmail.com>
2016-04-03 05:28:37 +0100

    added style/properties into packer

--- ts/export/packer/packer.ts
+++ ts/export/packer/packer.ts
@@ -9,10 +9,14 @@ export abstract class Packer {
     protected archive: any;
     private formatter: Formatter;
     protected document: Document;
+    private style: Style;
+    private properties: Properties;
 
     constructor(document: Document, style: Style, properties: Properties) {
         this.formatter = new Formatter();
         this.document = document;
+        this.style = style;
+        this.properties = properties;
         this.archive = archiver.create("zip", {});
 
         this.archive.on('error', (err) => {
@@ -32,6 +36,18 @@ export abstract class Packer {
         ]);
 
         //this.archive.directory(__dirname + "/template", "/");
+
+        this.archive.append(this.document, {
+            name: 'word/document.xml'
+        });
+
+        this.archive.append(this.style, {
+            name: 'word/newStyle.xml'
+        });
+
+        this.archive.append(this.properties, {
+            name: 'docProps/core.xml'
+        });
 
         this.archive.finalize();
     }
