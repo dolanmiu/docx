d197a2c71722585cae00dd8dd0133fd2aec68428
Dolan Miu <dolan_miu@hotmail.com>
2016-04-05 01:48:47 +0100

    xml typing

--- ts/typings/tsd.d.ts
+++ ts/typings/tsd.d.ts
@@ -9,3 +9,4 @@
 /// <reference path="express/express.d.ts" />
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="xml/xml.d.ts" />
