bf061c6567503d437407b3673df8bd8fa810b0b2
Dolan Miu <dolan_miu@hotmail.com>
2016-05-10 00:41:36 +0100

    added more styles

--- ts/styles/factory.ts
+++ ts/styles/factory.ts
@@ -2,11 +2,11 @@ import {Styles} from "./";
 import {DocumentDefaults} from "./defaults";
 import {ParagraphPropertiesDefaults} from "./defaults/paragraph-properties";
 import {RunPropertiesDefaults} from "./defaults/run-properties";
-import {Heading1Style, Heading2Style, TitleStyle} from "./style";
+import {Heading1Style, Heading2Style, TitleStyle, Heading3Style, Heading4Style, Heading5Style, Heading6Style} from "./style";
 //import {StyleAttributes} from "./style/attributes";
 import {ParagraphProperties} from "../docx/paragraph/properties";
 import {RunProperties} from "../docx/run/properties";
-import {Color, Size} from "../docx/run/formatting";
+import {Color, Size, Italics} from "../docx/run/formatting";
 
 export class DefaultStylesFactory {
 
@@ -27,6 +27,24 @@ export class DefaultStylesFactory {
         heading2Style.addRunProperty(new Color("2E74B5"));
         heading2Style.addRunProperty(new Size(26));
         styles.push(heading2Style);
+
+        var heading3Style = new Heading3Style();
+        heading3Style.addRunProperty(new Color("1F4D78"));
+        heading3Style.addRunProperty(new Size(24));
+        styles.push(heading3Style);
+
+        var heading4Style = new Heading4Style();
+        heading4Style.addRunProperty(new Color("2E74B5"));
+        heading4Style.addRunProperty(new Italics());
+        styles.push(heading4Style);
+
+        var heading5Style = new Heading5Style();
+        heading5Style.addRunProperty(new Color("2E74B5"));
+        styles.push(heading5Style);
+
+        var heading6Style = new Heading6Style();
+        heading6Style.addRunProperty(new Color("1F4D78"));
+        styles.push(heading6Style);
 
         console.log(JSON.stringify(styles, null, "  "));
         return styles;
