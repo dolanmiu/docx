57680159fc1f90a2b38dbe2c91d4909c4b715373
Dolan Miu <dolan_miu@hotmail.com>
2016-05-18 17:09:50 +0100

    made express packer work

--- ts/export/packer/express.ts
+++ ts/export/packer/express.ts
@@ -2,12 +2,27 @@ import {Packer} from "./packer";
 import * as fs from "fs";
 import * as express from "express";
 import {Document} from "../../docx/document";
+import {Properties} from "../../properties";
+import {DefaultStylesFactory} from "../../styles/factory"
 
 export class ExpressPacker extends Packer {
     private res: express.Response;
 
-    constructor(document: Document, res: express.Response) {
-        super(document, null, null);
+    constructor(document: Document, res: express.Response, styles?: any, properties?: Properties) {
+        if (!styles) {
+            var stylesFactory = new DefaultStylesFactory();
+            styles = stylesFactory.newInstance()
+        }
+
+        if (!properties) {
+            properties = new Properties({
+                creator: "Shan Fu",
+                revision: "1",
+                lastModifiedBy: "Shan Fu"
+            });
+        }
+        
+        super(document, styles, properties);
         this.res = res;
 
         this.res.on('close', () => {
@@ -15,7 +30,7 @@ export class ExpressPacker extends Packer {
         });
     }
 
-    pack() {
+    pack(name: string): void {
         this.res.attachment(name + ".docx");
         super.pack(this.res);
     }
