606e03d1563d645e6ce9a52196a64c8143aea927
Dolan Miu <dolan_miu@hotmail.com>
2016-05-19 23:06:15 +0100

    added paragraph and run property xml components

--- ts/docx/xml-components/index.ts
+++ ts/docx/xml-components/index.ts
@@ -124,11 +124,60 @@ export class Attributes extends XmlAttributeComponent {
             footer: "w:footer",
             gutter: "w:gutter",
             linePitch: "w:linePitch"
-        });
-        this.root = properties
-
-        if (!properties) {
-            this.root = {};
-        }
+        }, properties);
     }
-} 
+}
+
+export class ParagraphPropertyXmlComponent extends XmlComponent {
+    private paragraphProperties: ParagraphProperties;
+
+    constructor(rootKey) {
+        super(rootKey);
+        this.paragraphProperties = new ParagraphProperties();
+        this.root.push(this.paragraphProperties);
+    }
+
+    clearVariables(): void {
+        this.paragraphProperties.clearVariables();
+
+        delete this.paragraphProperties;
+    }
+}
+
+export class RunPropertyXmlComponent extends XmlComponent {
+    private runProperties: RunProperties;
+
+    constructor(rootKey) {
+        super(rootKey);
+        this.runProperties = new RunProperties();
+        this.root.push(this.runProperties);
+    }
+
+    clearVariables(): void {
+        this.runProperties.clearVariables();
+
+        delete this.runProperties;
+    }
+}
+
+export class MultiPropertyXmlComponent extends XmlComponent {
+    private runProperties: RunProperties;
+    private paragraphProperties: ParagraphProperties;
+
+    constructor(rootKey) {
+        super(rootKey);
+        this.runProperties = new RunProperties();
+        this.root.push(this.runProperties);
+        
+        this.paragraphProperties = new ParagraphProperties();
+        this.root.push(this.paragraphProperties);
+    }
+
+    clearVariables(): void {
+        this.runProperties.clearVariables();
+        this.paragraphProperties.clearVariables();
+
+        delete this.runProperties;
+        delete this.paragraphProperties;
+    }
+}
