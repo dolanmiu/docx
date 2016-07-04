2027391d50256bf3f82cfb0c9038e7a5982770b9
Dolan Miu <dolan_miu@hotmail.com>
2016-07-04 18:46:43 +0100

    added defaults to packer

--- ts/export/packer/express.ts
+++ ts/export/packer/express.ts
@@ -3,30 +3,12 @@ import * as fs from "fs";
 import * as express from "express";
 import {Document} from "../../docx/document";
 import {Properties} from "../../properties";
-import {DefaultStylesFactory} from "../../styles/factory";
 import {Numbering} from "../../numbering";
 
 export class ExpressPacker extends Packer {
     private res: express.Response;
 
     constructor(document: Document, res: express.Response, styles?: any, properties?: Properties, numbering?: Numbering) {
-        if (!styles) {
-            let stylesFactory = new DefaultStylesFactory();
-            styles = stylesFactory.newInstance();
-        }
-
-        if (!properties) {
-            properties = new Properties({
-                creator: "Shan Fu",
-                revision: "1",
-                lastModifiedBy: "Shan Fu"
-            });
-        }
-
-        if (!numbering) {
-            numbering = new Numbering();
-        }
-
         super(document, styles, properties, numbering);
         this.res = res;
 
