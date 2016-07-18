2ad04671fad0214063fe2e1e1fb9596501f730bf
Dolan Miu <dolan_miu@hotmail.com>
2016-07-18 19:23:35 +0100

    removed unused file

--- scripts/test-prepare.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-var data = fs.readFileSync('build/tests/tests/docxTest.js'); //read existing contents into data
-var fd = fs.openSync('build/tests/tests/docxTest.js', 'w+');
-var buffer = new Buffer('var docx = require("../docx/docx");');
-fs.writeSync(fd, buffer, 0, buffer.length); //write new data
-fs.writeSync(fd, data, 0, data.length); //append old data
-fs.close(fd);
