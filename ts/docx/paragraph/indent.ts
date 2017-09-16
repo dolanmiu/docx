195c305434d2fb96c4dfe183805cf5341430eb27
Jacob Wright <jacwright@gmail.com>
2017-09-16 16:59:01 -0600

    Adding the ability to indent first line only in paragraphs This is a backwards compatible fix while a better (but breaking) fix is here https://github.com/dolanmiu/docx/pull/44.

--- ts/docx/paragraph/indent.ts
+++ ts/docx/paragraph/indent.ts
@@ -3,22 +3,25 @@ import { XmlAttributeComponent, XmlComponent } from "../xml-components";
 interface IIndentAttributesProperties {
     left?: number;
     hanging?: number;
+    firstLine?: number;
 }
 
 class IndentAttributes extends XmlAttributeComponent<IIndentAttributesProperties> {
     protected xmlKeys = {
         left: "w:left",
         hanging: "w:hanging",
+        firstLine: "w:firstLine",
     };
 }
 
 export class Indent extends XmlComponent {
 
-    constructor(left: number, hanging?: number) {
+    constructor(left: number, hanging?: number, firstLine?: number) {
         super("w:ind");
         this.root.push(new IndentAttributes({
             left: left,
             hanging: hanging,
+            firstLine: firstLine,
         }));
     }
 }
