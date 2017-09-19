518fec0595d228b62143ff7481b0439adec20f21
Dolan <dolan_miu@hotmail.com>
2017-09-19 15:47:29 +0100

    Export like standard style

--- ts/export/index.ts
+++ ts/export/index.ts
@@ -1,3 +1,3 @@
-export { LocalPacker } from "./packer/local";
-export { ExpressPacker } from "./packer/express";
-export { Packer } from "./packer/packer";
+export * from "./packer/local";
+export * from "./packer/express";
+export * from "./packer/packer";
