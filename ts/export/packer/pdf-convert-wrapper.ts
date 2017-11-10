fb08f793447c248cb0cedcf697b2a572cc3e04b6
Dolan <dolan_miu@hotmail.com>
2017-11-10 14:27:57 +0000

    Add initial conversion

--- ts/export/packer/pdf-convert-wrapper.ts
+++ ts/export/packer/pdf-convert-wrapper.ts
@@ -1,5 +1,40 @@
+import * as fs from "fs";
 import * as request from "request";
 
-export class PdfConvertWrapper {
+export interface IConvertOutput {
+    data: string;
+}
 
+export class PdfConvertWrapper {
+    public convert(): Promise<IConvertOutput> {
+        const buffer = fs.readFileSync("test.docx");
+
+        return new Promise<IConvertOutput>((resolve, reject) => {
+            const r = request.post({
+                url: "http://mirror1.convertonlinefree.com",
+                encoding: null,
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36",
+                },
+            }, (err, response, body: IConvertOutput) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(body);
+            });
+
+            const form = r.form();
+            form.append("__EVENTTARGET", "");
+            form.append("__EVENTARGUMENT", "");
+            form.append("__VIEWSTATE", "");
+            form.append("ctl00$MainContent$fu", buffer, {
+                filename: "output.docx",
+                contentType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            });
+
+            form.append("ctl00$MainContent$btnConvert", "Convert");
+            form.append("ctl00$MainContent$fuZip", "");
+        });
+    }
 }
