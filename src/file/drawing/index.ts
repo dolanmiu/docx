998fe3f37017dda0a61cceda0cdb0d5007ec94ec
Dolan Miu <dolan_miu@hotmail.com>
2017-12-30 20:59:05 +0000

    Made index into barrel and made seperate drawing file

--- src/file/drawing/index.ts
+++ src/file/drawing/index.ts
@@ -1,16 +1,1 @@
-import { IData } from "file/media";
-import { XmlComponent } from "file/xml-components";
-import { Inline } from "./inline";
-
-export class Drawing extends XmlComponent {
-
-    constructor(imageData: IData) {
-        super("w:drawing");
-
-        if (imageData === undefined) {
-            throw new Error("imageData cannot be undefined");
-        }
-
-        this.root.push(new Inline(imageData.referenceId));
-    }
-}
+export { Drawing } from "./drawing";
