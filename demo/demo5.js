ccd655ef8be3828f2c4b1feb3517a905f98409d9
Dolan <dolan_miu@hotmail.com>
2018-01-22 22:12:33 +0000

    Add support for jpg and bmp

--- demo/demo5.js
+++ demo/demo5.js
@@ -7,6 +7,8 @@ doc.addParagraph(paragraph);
 
 const image = doc.createImage("./demo/images/image1.jpeg");
 const image2 = doc.createImage("./demo/images/dog.png");
+const image3 = doc.createImage("./demo/images/cat.jpg");
+const image4 = doc.createImage("./demo/images/parrots.bmp");
 
 var exporter = new docx.LocalPacker(doc);
 exporter.pack('My Document');
