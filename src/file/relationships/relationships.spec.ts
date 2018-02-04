fd93c0776a2449117d0320283fe72b4d535557a0
Dolan Miu <dolan_miu@hotmail.com>
2018-02-04 01:43:10 +0000

    Fix tests

--- src/file/relationships/relationships.spec.ts
+++ src/file/relationships/relationships.spec.ts
@@ -14,17 +14,6 @@ describe("Relationships", () => {
             expect(tree["Relationships"][0]).to.deep.equal({
                 _attr: { xmlns: "http://schemas.openxmlformats.org/package/2006/relationships" },
             });
-            expect(tree["Relationships"][1]).to.deep.equal({
-                Relationship: [
-                    {
-                        _attr: {
-                            Id: "rId1",
-                            Type: "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles",
-                            Target: "styles.xml",
-                        },
-                    },
-                ],
-            });
         });
     });
 });
