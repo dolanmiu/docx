1a1c1f26d9dc7192283f1ae55bb9fb07f0a0eb1c
Dolan <dolan_miu@hotmail.com>
2018-03-22 22:55:33 +0000

    Border for tables by default

--- src/file/table/properties.ts
+++ src/file/table/properties.ts
@@ -5,6 +5,7 @@ export type WidthTypes = "dxa" | "pct" | "nil" | "auto";
 export class TableProperties extends XmlComponent {
     constructor() {
         super("w:tblPr");
+        this.root.push(new TableBorders());
     }
 
     public setWidth(type: WidthTypes, w: number | string): TableProperties {
@@ -45,4 +46,39 @@ class TableLayout extends XmlComponent {
         super("w:tblLayout");
         this.root.push(new TableLayoutAttributes({ type }));
     }
+}
+
+class TableBorders extends XmlComponent {
+    constructor() {
+        super("w:tblBorders");
+        this.root.push(new TableBordersElement("w:top", "single", 4, 0, "auto"));
+        this.root.push(new TableBordersElement("w:left", "single", 4, 0, "auto"));
+        this.root.push(new TableBordersElement("w:bottom", "single", 4, 0, "auto"));
+        this.root.push(new TableBordersElement("w:right", "single", 4, 0, "auto"));
+        this.root.push(new TableBordersElement("w:insideH", "single", 4, 0, "auto"));
+        this.root.push(new TableBordersElement("w:insideV", "single", 4, 0, "auto"));
+    }
+}
+
+class TableBordersElement extends XmlComponent {
+    constructor(elementName: string, value: string, size: number, space: number, color: string) {
+        super(elementName);
+        this.root.push(
+            new TableBordersAttributes({
+                value,
+                size,
+                space,
+                color,
+            }),
+        );
+    }
+}
+
+class TableBordersAttributes extends XmlAttributeComponent<{ value: string; size: number; space: number; color: string }> {
+    protected xmlKeys = {
+        value: "w:val",
+        size: "w:sz",
+        space: "w:space",
+        color: "w:color",
+    };
 }
