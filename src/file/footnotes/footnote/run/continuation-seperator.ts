044442b0d73619d592d2a6472d16597c23b2cf0f
Dolan <dolan_miu@hotmail.com>
2018-06-25 19:50:19 +0100

    Add footnote classes

---
+++ src/file/footnotes/footnote/run/continuation-seperator.ts
@@ -0,0 +1,7 @@
+import { XmlComponent } from "file/xml-components";
+
+export class ContinuationSeperator extends XmlComponent {
+    constructor() {
+        super("w:continuationSeparator");
+    }
+}
---
+++ src/file/footnotes/footnote/run/reference-run.ts
@@ -0,0 +1,35 @@
+import { Run } from "file/paragraph/run";
+import { Style } from "file/paragraph/run/style";
+import { XmlAttributeComponent, XmlComponent } from "file/xml-components";
+
+export interface IFootNoteReferenceRunAttributesProperties {
+    id: number;
+}
+
+export class FootNoteReferenceRunAttributes extends XmlAttributeComponent<IFootNoteReferenceRunAttributesProperties> {
+    protected xmlKeys = {
+        id: "w:id",
+    };
+}
+
+export class FootnoteReference extends XmlComponent {
+    constructor(id: number) {
+        super("w:footnoteReference");
+
+        this.root.push(
+            new FootNoteReferenceRunAttributes({
+                id: id,
+            }),
+        );
+    }
+}
+
+export class FootnoteReferenceRun extends Run {
+    constructor(id: number) {
+        super();
+
+        this.properties.push(new Style("FootnoteReference"));
+
+        this.root.push(new FootnoteReference(id));
+    }
+}
