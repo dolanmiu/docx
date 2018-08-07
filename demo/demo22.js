fe43525dada52eb8805d95a986f09ed18123d792
amitm02 <amit.man@gmail.com>
2018-08-07 16:19:13 +0300

    update hebrew demo

--- demo/demo22.js
+++ demo/demo22.js
@@ -2,11 +2,23 @@ const docx = require('../build');
 
 var doc = new docx.Document();
 
-var textRun = new docx.TextRun("שלום עולם").rtl();
-var paragraph = new docx.Paragraph().bidi();
-paragraph.addRun(textRun);
 
-doc.addParagraph(paragraph);
+
+var paragraph1 = new docx.Paragraph().bidi();
+var textRun1 = new docx.TextRun("שלום עולם").rtl();
+paragraph1.addRun(textRun1);
+doc.addParagraph(paragraph1);
+
+var paragraph2 = new docx.Paragraph().bidi();
+var textRun2 = new docx.TextRun("שלום עולם").bold().rtl();
+paragraph2.addRun(textRun2);
+doc.addParagraph(paragraph2);
+
+var paragraph3 = new docx.Paragraph().bidi();
+var textRun3 = new docx.TextRun("שלום עולם").italic().rtl();
+paragraph3.addRun(textRun3);
+doc.addParagraph(paragraph3);
+
 
 var exporter = new docx.LocalPacker(doc);
 exporter.pack('My Document');
