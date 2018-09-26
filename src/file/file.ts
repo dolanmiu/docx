52b07fd9cb00de1492941d33b6bba2396832eb17
amitm02 <amit.man@gmail.com>
2018-09-26 10:12:40 +0300

    Revert "Add back relationships"
    This reverts commit 6e0c12afb3d6dff7951b5664b9155890de3de8ce.

--- src/file/file.ts
+++ src/file/file.ts
@@ -249,22 +249,6 @@ export class File {
             "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties",
             "docProps/app.xml",
         );
-
-        this.docRelationships.createRelationship(
-            this.currentRelationshipId++,
-            "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles",
-            "styles.xml",
-        );
-        this.docRelationships.createRelationship(
-            this.currentRelationshipId++,
-            "http://schemas.openxmlformats.org/officeDocument/2006/relationships/numbering",
-            "numbering.xml",
-        );
-        this.docRelationships.createRelationship(
-            this.currentRelationshipId++,
-            "http://schemas.openxmlformats.org/officeDocument/2006/relationships/footnotes",
-            "footnotes.xml",
-        );
     }
 
     public get Document(): Document {
