7fb2154ecb438a6bfbd74ba970d5a342c2f88c7a
Dolan <dolan_miu@hotmail.com>
2020-01-16 02:53:51 +0000

    Upgrade webpack and build two versions of docx
    Client and server

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@
 import * as path from "path";
 import { Configuration } from "webpack";
 
-module.exports = {
+const serverConfig = {
     entry: "./src/index.ts",
 
     output: {
@@ -21,7 +21,7 @@ module.exports = {
         rules: [
             {
                 test: /\.ts$/,
-                loaders: ["awesome-typescript-loader"],
+                loaders: ["ts-loader"],
             },
             // For coverage testing
             ...(process.env.NODE_ENV !== "production"
@@ -38,6 +38,44 @@ module.exports = {
         ],
     },
 
-    // Because docx is now targetting web
-    // target: 'node',
+    target: "node",
 } as Configuration;
+
+const clientConfig = {
+    entry: "./src/index.ts",
+
+    output: {
+        path: path.resolve("build"),
+        filename: "index.web.js",
+        libraryTarget: "umd",
+        library: "docx",
+    },
+
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        modules: [path.resolve("./src"), "node_modules"],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ["ts-loader"],
+            },
+            // For coverage testing
+            ...(process.env.NODE_ENV !== "production"
+                ? [
+                      {
+                          test: /\.(ts)/,
+                          include: path.resolve("src"),
+                          loader: "istanbul-instrumenter-loader",
+                          enforce: "post",
+                          exclude: [/node_modules/],
+                      },
+                  ]
+                : []),
+        ],
+    },
+} as Configuration;
+
+module.exports = [serverConfig, clientConfig];
