2adfe532dde74234e1f6665f85da4ae18f2775fb
Thomas Jansen <thojansen@gmail.com>
2020-09-23 12:59:55 +0200

    add more unit tests for trackRevision settings

--- src/file/settings/settings.spec.ts
+++ src/file/settings/settings.spec.ts
@@ -79,4 +79,47 @@ describe("Settings", () => {
             expect(keys[0]).to.be.equal("w:compat");
         });
     });
+    describe("#addTrackRevisions", () => {
+        it("should add an empty Track Revisions", () => {
+            const settings = new Settings();
+            settings.addTrackRevisions();
+
+            const tree = new Formatter().format(settings);
+            let keys: string[] = Object.keys(tree);
+            expect(keys[0]).to.be.equal("w:settings");
+            const rootArray = tree["w:settings"];
+            expect(rootArray).is.an.instanceof(Array);
+            expect(rootArray).has.length(2);
+            keys = Object.keys(rootArray[0]);
+            expect(keys).is.an.instanceof(Array);
+            expect(keys).has.length(1);
+            expect(keys[0]).to.be.equal("_attr");
+            keys = Object.keys(rootArray[1]);
+            expect(keys).is.an.instanceof(Array);
+            expect(keys).has.length(1);
+            expect(keys[0]).to.be.equal("w:trackRevisions");
+        });
+    });
+    describe("#addTrackRevisionsTwice", () => {
+        it("should add an empty Track Revisions if called twice", () => {
+            const settings = new Settings();
+            settings.addTrackRevisions();
+            settings.addTrackRevisions();
+
+            const tree = new Formatter().format(settings);
+            let keys: string[] = Object.keys(tree);
+            expect(keys[0]).to.be.equal("w:settings");
+            const rootArray = tree["w:settings"];
+            expect(rootArray).is.an.instanceof(Array);
+            expect(rootArray).has.length(2);
+            keys = Object.keys(rootArray[0]);
+            expect(keys).is.an.instanceof(Array);
+            expect(keys).has.length(1);
+            expect(keys[0]).to.be.equal("_attr");
+            keys = Object.keys(rootArray[1]);
+            expect(keys).is.an.instanceof(Array);
+            expect(keys).has.length(1);
+            expect(keys[0]).to.be.equal("w:trackRevisions");
+        });
+    });
 });
