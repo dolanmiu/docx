b2fea471f181b9b1de5f09330c3c25d9158275e7
Dolan Miu <dolan_miu@hotmail.com>
2020-11-15 23:03:44 +0000

    Add workaround to include file in build

--- src/file/paragraph/math/math-component.ts
+++ src/file/paragraph/math/math-component.ts
@@ -19,3 +19,9 @@ export type MathComponent =
     | MathCurlyBrackets
     | MathAngledBrackets
     | MathSquareBrackets;
+
+// Needed because of: https://github.com/s-panferov/awesome-typescript-loader/issues/432
+/**
+ * @ignore
+ */
+export const WORKAROUND4 = "";
