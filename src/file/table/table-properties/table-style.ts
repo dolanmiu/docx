0ec3b64ad7db0c1ca7bd3f9303df6c9e8f944905
Dolan <dolan_miu@hotmail.com>
2021-03-07 21:45:59 +0000

    Add tests

--- src/file/table/table-properties/table-style.ts
+++ src/file/table/table-properties/table-style.ts
@@ -1,11 +1,9 @@
 import { Attributes, XmlComponent } from "file/xml-components";
 
 export class TableStyle extends XmlComponent {
-    public readonly styleId: string;
-
     constructor(styleId: string) {
         super("w:tblStyle");
-        this.styleId = styleId;
+
         this.root.push(
             new Attributes({
                 val: styleId,
