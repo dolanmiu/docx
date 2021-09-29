d881646e73a0f5bd756a7de281611839a3d81b12
Dolan Miu <dolan_miu@hotmail.com>
2021-09-30 00:19:03 +0100

    Try using GitHub Action validator

--- scripts/schema-validator/index.ts
+++ scripts/schema-validator/index.ts
@@ -1,18 +1,10 @@
-import * as fs from "fs";
 import * as unzipper from "unzipper";
-import { validateXMLWithXSD } from "validate-with-xmllint";
 
 const main = async () => {
-    fs.copyFileSync("My Document.docx", "scripts/schema-validator/work-area/doc.zip");
-
-    const zip = await unzipper.Open.file("scripts/schema-validator/work-area/doc.zip");
+    const zip = await unzipper.Open.file("My Document.docx");
     await zip.extract({
-        path: "scripts/schema-validator/work-area/doc",
+        path: "build/extracted-doc",
     });
-
-    const xml = fs.readFileSync("scripts/schema-validator/work-area/doc/word/document.xml", "ascii");
-
-    await validateXMLWithXSD(xml, "scripts/schema-validator/schemas/microsoft/wml-2010.xsd");
 };
 
 main();
