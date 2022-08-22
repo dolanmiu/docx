f4f6ee50551acfdbd08b315bbd86f68145da9ffc
Matt Danner <MLD5000@users.noreply.github.com>
2022-08-22 15:26:52 -0500

    Created toStream example

---
+++ demo/74-nodejs-stream.ts
@@ -0,0 +1,31 @@
+// Simple example to add text to a document
+// Import from 'docx' rather than '../build' if you install from npm
+import * as fs from "fs";
+import { Document, Packer, Paragraph, TextRun } from "../build";
+
+const doc = new Document({
+    sections: [
+        {
+            properties: {},
+            children: [
+                new Paragraph({
+                    children: [
+                        new TextRun("Hello World"),
+                        new TextRun({
+                            text: "Foo Bar",
+                            bold: true,
+                        }),
+                        new TextRun({
+                            text: "\tGithub is the best",
+                            bold: true,
+                        }),
+                    ],
+                }),
+            ],
+        },
+    ],
+});
+
+Packer.toStream(doc).then((stream) => {
+    stream.pipe(fs.createWriteStream("My Document.docx"));
+});
