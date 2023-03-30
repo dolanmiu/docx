44568023cdd5d3cb0e01198064ab9e90fcf7e52e
Dolan Miu <dolan_miu@hotmail.com>
2023-03-30 19:54:33 +0100

    Add documentation to basedOn

--- src/file/styles/style/style.ts
+++ src/file/styles/style/style.ts
@@ -41,6 +41,13 @@ export interface IStyleAttributes {
 
 export interface IStyleOptions {
     readonly name?: string;
+    /**
+     * Specifies the style upon which the current style is based-that is, the style from which the current style inherits. It is the mechanism for implementing style inheritance.
+     * Note that if the type of the current style must match the type of the style upon which it is based or the basedOn element will be ignored.
+     * However, if the current style is a numbering style, then the `basedOn` element is ignored.
+     *
+     * **WARNING**: You cannot set `basedOn` to be the same as `name`. This is akin to inheriting from itself. This creates a cyclic dependency and cause undesirable behavior.
+     */
     readonly basedOn?: string;
     readonly next?: string;
     readonly link?: string;
