c57aaa856d5c7cc6d3d5928a07b2546168b775fb
Dolan Miu <dolan_miu@hotmail.com>
2023-03-30 19:43:09 +0100

    Update comments demo

--- demo/73-comments.ts
+++ demo/73-comments.ts
@@ -32,6 +32,55 @@ const doc = new Document({
                     }),
                 ],
             },
+            {
+                id: 1,
+                author: "Bob Ross",
+                date: new Date(),
+                children: [
+                    new Paragraph({
+                        children: [
+                            new TextRun({
+                                text: "Some initial text content",
+                            }),
+                        ],
+                    }),
+                    new Paragraph({
+                        children: [
+                            new TextRun({
+                                text: "comment text content",
+                            }),
+                        ],
+                    }),
+                ],
+            },
+            {
+                id: 2,
+                author: "John Doe",
+                date: new Date(),
+                children: [
+                    new Paragraph({
+                        children: [
+                            new TextRun({
+                                text: "Hello World",
+                            }),
+                        ],
+                    }),
+                ],
+            },
+            {
+                id: 3,
+                author: "Beatriz",
+                date: new Date(),
+                children: [
+                    new Paragraph({
+                        children: [
+                            new TextRun({
+                                text: "Another reply",
+                            }),
+                        ],
+                    }),
+                ],
+            },
         ],
     },
     sections: [
@@ -49,6 +98,32 @@ const doc = new Document({
                         new CommentRangeEnd(0),
                         new TextRun({
                             children: [new CommentReference(0)],
+                            bold: true,
+                        }),
+                    ],
+                }),
+                new Paragraph({
+                    children: [
+                        new CommentRangeStart(1),
+                        new CommentRangeStart(2),
+                        new CommentRangeStart(3),
+                        new TextRun({
+                            text: "Some text which need commenting",
+                            bold: true,
+                        }),
+                        new CommentRangeEnd(1),
+                        new TextRun({
+                            children: [new CommentReference(1)],
+                            bold: true,
+                        }),
+                        new CommentRangeEnd(2),
+                        new TextRun({
+                            children: [new CommentReference(2)],
+                            bold: true,
+                        }),
+                        new CommentRangeEnd(3),
+                        new TextRun({
+                            children: [new CommentReference(3)],
                             bold: true,
                         }),
                     ],
