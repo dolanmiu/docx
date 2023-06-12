28ad740473b5d6775406eb345766eb5d7195ab64
Dolan Miu <dolan_miu@hotmail.com>
2023-06-12 19:21:22 +0100

    Fix linting

--- demo/72-word-wrap.ts
+++ demo/72-word-wrap.ts
@@ -1,7 +1,7 @@
 // Example on how to preserve word wrap text. Works with all languages.
 
 import * as fs from "fs";
-import { Document, Packer, Paragraph, TextRun, SpaceType } from "docx";
+import { Document, Packer, Paragraph, TextRun } from "docx";
 
 const doc = new Document({
     sections: [
