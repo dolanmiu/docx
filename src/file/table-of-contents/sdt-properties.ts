20e0213c7d4d1eb2c13465da1f18d3f26ea67bb5
Juan D <ipsof116@gmail.com>
2023-06-22 15:37:00 -0400

    Revert "Revert unnecessary changes to sdt-properties"
    This reverts commit d1f75e3a422a6b5109e9894bc5edf2c45a9c112e.

--- src/file/table-of-contents/sdt-properties.ts
+++ src/file/table-of-contents/sdt-properties.ts
@@ -1,10 +1,25 @@
 // http://www.datypic.com/sc/ooxml/e-w_sdtPr-1.html
+import { CheckBoxUtil } from "@file/checkbox";
 import { StringValueElement, XmlComponent } from "@file/xml-components";
 
+export type SdtPrChild =
+    | CheckBoxUtil;
+
+export interface ISdtPropertiesOptions {
+    readonly children : readonly SdtPrChild[];
+}
 export class StructuredDocumentTagProperties extends XmlComponent {
-    public constructor(alias: string) {
+    public constructor(alias?: string, options?: ISdtPropertiesOptions) {
         super("w:sdtPr");
 
-        this.root.push(new StringValueElement("w:alias", alias));
+        if(typeof alias === 'string'){
+            this.root.push(new StringValueElement("w:alias", alias));
+        }
+
+        if(options?.children){
+            for(const child of options.children){
+                this.root.push(child)
+            }
+        }
     }
 }
