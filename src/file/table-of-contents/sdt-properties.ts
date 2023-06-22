58e7dbf445f5cd8779d7520a32b686a5b86fc20d
Juan D <ipsof116@gmail.com>
2023-06-22 15:39:39 -0400

    Revert "Modify sdt-properties to accommodate for children elements"
    This reverts commit f706d8e62d0068d14aac576e936fceaa93b60601.

--- src/file/table-of-contents/sdt-properties.ts
+++ src/file/table-of-contents/sdt-properties.ts
@@ -1,25 +1,9 @@
 // http://www.datypic.com/sc/ooxml/e-w_sdtPr-1.html
-import { CheckBoxUtil } from "@file/checkbox";
 import { StringValueElement, XmlComponent } from "@file/xml-components";
 
-export type SdtPrChild =
-    | CheckBoxUtil;
-
-export interface ISdtPropertiesOptions {
-    readonly children : readonly SdtPrChild[];
-}
 export class StructuredDocumentTagProperties extends XmlComponent {
-    public constructor(alias?: string, options?: ISdtPropertiesOptions) {
+    public constructor(alias: string) {
         super("w:sdtPr");
-
-        if(typeof alias === 'string'){
-            this.root.push(new StringValueElement("w:alias", alias));
-        }
-
-        if(options?.children){
-            for(const child of options.children){
-                this.root.push(child)
-            }
-        }
+        this.root.push(new StringValueElement("w:alias", alias));
     }
 }
