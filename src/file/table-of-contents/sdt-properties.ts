9e9ca526fec4ab851bb6ba9293442338c9aa72d9
Juan D <ipsof116@gmail.com>
2023-06-23 13:05:21 -0400

    Prettier syntax update of sdt-properties

--- src/file/table-of-contents/sdt-properties.ts
+++ src/file/table-of-contents/sdt-properties.ts
@@ -4,7 +4,7 @@ import { StringValueElement, XmlComponent } from "@file/xml-components";
 export class StructuredDocumentTagProperties extends XmlComponent {
     public constructor(alias?: string) {
         super("w:sdtPr");
-        if(typeof alias === 'string'){
+        if (typeof alias === "string") {
             this.root.push(new StringValueElement("w:alias", alias));
         }
     }
