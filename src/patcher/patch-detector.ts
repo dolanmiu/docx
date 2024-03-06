bea6a1644c2baad3cc090f5063aaa94628fc3aa1
Christopher Fox <cfox@homebound.com>
2024-03-05 17:16:09 -0700

    feat: Add ability to detect patches which are present in a file

---
+++ src/patcher/patch-detector.ts
@@ -0,0 +1,27 @@
+import JSZip from "jszip";
+import { toJson } from "./util";
+import { traverse } from "./traverser";
+import { InputDataType } from "./from-docx";
+
+export type PatchDetectorOptions = {
+    readonly data: InputDataType;
+};
+
+/** Detects which patches are needed/present in a template */
+export const patchDetector = async ({ data }: PatchDetectorOptions): Promise<readonly string[]> => {
+    const zipContent = await JSZip.loadAsync(data);
+    const patches = new Set<string>();
+
+    for (const [key, value] of Object.entries(zipContent.files)) {
+        if (key.startsWith("word/") && !key.endsWith(".xml.rels")) {
+            const json = toJson(await value.async("text"));
+            traverse(json).forEach((p) => findPatchKeys(p.text).forEach((patch) => patches.add(patch)));
+        }
+    }
+    return Array.from(patches);
+};
+
+const findPatchKeys = (text: string): readonly string[] => {
+    const pattern = /(?<=\{\{).+?(?=\}\})/gs;
+    return text.match(pattern) ?? [];
+};
