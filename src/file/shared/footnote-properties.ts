7b2e8ede07a1ebcc0d02d8a1a8906119e3323cf5
Táborszki Bálint <taborszkib@gmail.com>
2024-05-04 01:54:46 +0200

    Switch enums to objects with as const

--- src/file/shared/footnote-properties.ts
+++ src/file/shared/footnote-properties.ts
@@ -1,16 +1,16 @@
-export enum FootnoteRestartLocation {
-     Continuous ="continuous",
-     EachSection = "eachSect",
-     EachPage = "eachPage",
-}
+export const FootnoteRestartLocation = {
+     CONTINUOUS: "continuous",
+     EACH_SECTION: "eachSect",
+     EACH_PAGE: "eachPage",
+} as const;
 
 export type FootnoteRestartLocationType = (typeof FootnoteRestartLocation)[keyof typeof FootnoteRestartLocation]
 
-export enum FootnotePositioningLocation {
-    PageBottom = "pageBottom",
-    BeneathText = "beneathText",
-    SectionEnd = "sectEnd",
-    DocumentEnd = "docEnd",
-}
+export const FootnotePositioningLocation = {
+    PAGE_BOTTOM: "pageBottom",
+    BENEATH_TEXT: "beneathText",
+    SECTION_END: "sectEnd",
+    DOCUMENT_END: "docEnd",
+} as const;
 
 export type FootnotePositioningLocationType = (typeof FootnotePositioningLocation)[keyof typeof FootnotePositioningLocation]
