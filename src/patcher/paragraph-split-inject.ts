6f3c748fd0c04d192461d659d36eed724daeec61
Dolan Miu <dolan_miu@hotmail.com>
2024-10-13 00:42:35 +0100

    fix: Missing text property
    Ignore <br /> for patched files

--- src/patcher/paragraph-split-inject.ts
+++ src/patcher/paragraph-split-inject.ts
@@ -29,7 +29,7 @@ export const splitRunElement = (runElement: Element, token: string): { readonly 
         runElement.elements
             ?.map((e, i) => {
                 if (e.type === "element" && e.name === "w:t") {
-                    const text = (e.elements?.[0].text as string) ?? "";
+                    const text = (e.elements?.[0]?.text as string) ?? "";
                     const splitText = text.split(token);
                     const newElements = splitText.map((t) => ({
                         ...e,
