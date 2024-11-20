dece0f58e151360936235a5bdf4e4902c7486c6e
SebKranz <33868262+SebKranz@users.noreply.github.com>
2024-11-20 12:19:25 +0100

    Fix duplicated br tags (#2717)
    * Adjust test to demo duplicated br tags
    
    * Fix patchDocument duplicating br tags
    
    * Only include w:Pr
    
    * Fix tag condition
    
    ---------
    
    Co-authored-by: Dolan <dolan_miu@hotmail.com>

--- src/patcher/replacer.ts
+++ src/patcher/replacer.ts
@@ -64,7 +64,7 @@ export const replacer = ({
 
                 if (keepOriginalStyles) {
                     const runElementNonTextualElements = runElementToBeReplaced.elements!.filter(
-                        (e) => e.type === "element" && e.name !== "w:t" && e.name !== "w:br" && e.name !== "w:tab",
+                        (e) => e.type === "element" && e.name === "w:rPr",
                     );
 
                     newRunElements = textJson.map((e) => ({
