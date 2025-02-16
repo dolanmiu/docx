ac2e29f5c9e59affe3b634b3262de4cf5ce8e9bb
Dolan Miu <dolan_miu@hotmail.com>
2025-02-16 18:26:29 +0000

    Fix prettier

--- demo/95-paragraph-style-with-shading-and-borders.ts
+++ demo/95-paragraph-style-with-shading-and-borders.ts
@@ -5,45 +5,45 @@ const doc = new Document({
     styles: {
         paragraphStyles: [
             {
-                id: 'withSingleBlackBordersAndYellowShading',
-                name: 'Paragraph Style with Black Borders and Yellow Shading',
-                basedOn: 'Normal',
+                id: "withSingleBlackBordersAndYellowShading",
+                name: "Paragraph Style with Black Borders and Yellow Shading",
+                basedOn: "Normal",
                 paragraph: {
                     shading: {
-                        color: '#fff000',
-                        type: 'solid'
+                        color: "#fff000",
+                        type: "solid",
                     },
                     border: {
                         top: {
                             style: BorderStyle.SINGLE,
-                            color: '#000000',
+                            color: "#000000",
                             size: 4,
                         },
                         bottom: {
                             style: BorderStyle.SINGLE,
-                            color: '#000000',
+                            color: "#000000",
                             size: 4,
                         },
                         left: {
                             style: BorderStyle.SINGLE,
-                            color: '#000000',
+                            color: "#000000",
                             size: 4,
                         },
                         right: {
                             style: BorderStyle.SINGLE,
-                            color: '#000000',
+                            color: "#000000",
                             size: 4,
-                        }
-                    }
-                }
-            }
-        ]
+                        },
+                    },
+                },
+            },
+        ],
     },
     sections: [
         {
             children: [
                 new Paragraph({
-                    style: 'withSingleBlackBordersAndYellowShading',
+                    style: "withSingleBlackBordersAndYellowShading",
                     children: [
                         new TextRun({
                             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
