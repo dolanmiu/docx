09fb2d06807f514579dfddd4477078f93fc81901
Alexander Nortung <alexander.nortung@oakdigital.dk>
2025-12-12 13:44:16 +0100

    chore(table-of-contents): clean up
    This commit can be squashed with the previous one if necessary

--- src/file/table-of-contents/table-of-contents.ts
+++ src/file/table-of-contents/table-of-contents.ts
@@ -25,68 +25,74 @@ export class TableOfContents extends FileChild {
 
         const content = new StructuredDocumentTagContent();
 
-        const beginParagraphChildren =
-            cachedContent.length >= 1 ? [this.buildCachedContentParagraphChild(cachedContent[0], properties)] : [];
+        const beginParagraphMandatoryChidlren = [
+            new Run({
+                children: [new Begin(true), new FieldInstruction(properties), new Separate()],
+            }),
+        ];
 
-        const getTabStops = (level: number): readonly TabStopDefinition[] => {
-            const levelSpace = 720;
-            const levelPosition = 9026 - (level - 1) * levelSpace; // TODO: should be equal to page width + 1 - level margin
-            return [
-                {
-                    type: "clear",
-                    position: levelPosition,
-                },
-                {
-                    type: "right",
-                    position: 9025, // TODO: should be equal to page width
-                    leader: "dot",
-                },
-            ];
-        };
+        const endParagraphMandatoryChildren = [
+            new Run({
+                children: [new End()],
+            }),
+        ];
 
-        const beginParagraph = new Paragraph({
-            style: cachedContent.length >= 1 ? `TOC${cachedContent[0].level}` : undefined,
-            tabStops: getTabStops(cachedContent.length >= 1 ? cachedContent[0].level : 1),
-            children: [
-                new Run({
-                    children: [new Begin(true), new FieldInstruction(properties), new Separate()],
-                }),
-                ...beginParagraphChildren,
-            ],
+        const cachedParagraphs = cachedContent.map((entry, i) => {
+            const contentChild = this.buildCachedContentParagraphChild(entry, properties);
+            const children =
+                i === 0
+                    ? [...beginParagraphMandatoryChidlren, contentChild]
+                    : i === cachedContent.length - 1
+                      ? [contentChild, ...endParagraphMandatoryChildren]
+                      : [contentChild];
+
+            return new Paragraph({
+                style: `TOC${entry.level}`,
+                tabStops: this.getTabStopsForLevel(entry.level),
+                children,
+            });
         });
 
-        content.addChildElement(beginParagraph);
-
-        const middleParagraphs = cachedContent.slice(1, -1).map(
-            (entry) =>
+        let paragraphs = cachedParagraphs;
+        if (cachedContent.length <= 0) {
+            paragraphs = [
                 new Paragraph({
-                    style: `TOC${entry.level}`,
-                    tabStops: getTabStops(entry.level),
-                    children: [this.buildCachedContentParagraphChild(entry, properties)],
+                    children: beginParagraphMandatoryChidlren,
                 }),
-        );
+                new Paragraph({
+                    children: endParagraphMandatoryChildren,
+                }),
+            ];
+        } else if (cachedContent.length <= 1) {
+            paragraphs = [
+                ...cachedParagraphs,
+                new Paragraph({
+                    children: endParagraphMandatoryChildren,
+                }),
+            ];
+        }
 
-        for (const paragraph of middleParagraphs) {
+        for (const paragraph of paragraphs) {
             content.addChildElement(paragraph);
         }
 
-        const endParagraphChildren =
-            cachedContent.length > 1 ? [this.buildCachedContentParagraphChild(cachedContent[cachedContent.length - 1], properties)] : [];
-
-        const endParagraph = new Paragraph({
-            style: cachedContent.length > 1 ? `TOC${cachedContent[cachedContent.length - 1].level}` : undefined,
-            tabStops: getTabStops(cachedContent.length > 1 ? cachedContent[cachedContent.length - 1].level : 1),
-            children: [
-                ...endParagraphChildren,
-                new Run({
-                    children: [new End()],
-                }),
-            ],
-        });
-
-        content.addChildElement(endParagraph);
-
         this.root.push(content);
+    }
+
+    private getTabStopsForLevel(level: number, pageWidth: number = 9025): readonly TabStopDefinition[] {
+        const levelSpace = 720;
+        const levelPosition = pageWidth + 1 - (level - 1) * levelSpace; // TODO: should be equal to page width + 1 - level margin
+        return [
+            {
+                type: "clear",
+                position: levelPosition,
+            },
+            {
+                type: "right",
+                position: pageWidth,
+                leader: "dot",
+            },
+        ];
     }
 
     private buildCachedContentRun(entry: ToCEntry, properties?: ITableOfContentsOptions): Run {
